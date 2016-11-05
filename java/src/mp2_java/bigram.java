b4282690c88d4d3b03ebf5b0ea7b0492e7d7d3ed
Pedro Ascensao <rafascen@gmail.com>
2016-11-05 14:29:34 +0000

    Deleted java

--- java/src/mp2_java/bigram.java
+++
@@ -1,8 +0,0 @@
-package mp2_java;
-
-public class bigram{
-	
-	String word1;
-	String word2;
-	int count;
-}
--- java/src/mp2_java/unigram.java
+++
@@ -1,7 +0,0 @@
-package mp2_java;
-
-public class unigram {
-	
-	String word;
-	int count;
-}
