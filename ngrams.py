b7ffc035dc5acc40c5e9c700aac9ee8ff83fd585
Pedro Ascensao <rafascen@gmail.com>
2016-11-06 00:00:10 +0000

    gotta calculate those probs

--- ngrams.py
+++ ngrams.py
@@ -1,6 +1,5 @@
 import sys
-import json
-import pickle
+import os
 
 # not tested, and somethings may be missing
 
@@ -54,13 +53,31 @@ def unigrams(phr):
 	
    return unigramsCount 
 
-#def loadUnigrams(author):
-#	with open( 
+def loadUnigrams(author):
+	unigrams = {}
+	f = "target/unigrams-" + author + ".txt" 
+	with open(f, 'r') as i:
+		lines = i.readlines()
+		for line in lines:
+			words = line.split()
+			unigrams[words[0]]=int(words[1])
+	return unigrams
 
-def splitPhrases(file):
+def loadBigrams(author):
+	bigrams = {"<s>": {}}
+	f = "target/bgrams-" + author + ".txt"
+	with open(f, 'r') as i:
+		lines = i.readlines()
+		for line in lines:
+			words = line.split()
+			bigrams[words[0]] = {words[1] : 0}
+			bigrams[words[0]][words[1]] = int(words[2])
+	return bigrams 
+
+def splitPhrases(f):
    phrases = []
    delimiter = ['.','!','?']
-   with open(file, 'r') as i:
+   with open(f, 'r') as i:
        text = i.read().replace('\n',' ').lower()
        words = text.split()
        phrase = ""
@@ -73,12 +90,111 @@ def splitPhrases(file):
  		phrase = phrase + word + " " 
    return phrases
    
-def laplace(phr):
-	unigramsCount = unigrams(p)
-	bigramsCount = bigrams(p)
-	
-	difwords = len(unigramsCount)
+def getNumWords(unigrams):
+	counter = 0
+	for key in unigrams:
+		counter = counter + unigrams[key]
+	return counter
 
+def getUniqueWords(unigrams):
+	counter = 0
+	for key in unigrams:
+		counter = counter + 1
+	return counter - 2
+		
+def probUni(phr):
+    authorsList = getAuthors()
+    probs = {}
+
+    for a in authorsList:
+	probs[a] = 0
+
+    for f in phr:
+	p = prob_phr_uni(f)
+	for author in probs:
+		probs[author] = probs[author] + p[author]
+      
+    maximum = -sys.maxint - 1
+    chosenOne = ""
+    for a in authorsList:
+	if probs[a] > maximum:
+		maximum = probs[a]
+		chosenOne = a
+    print probs
+    print chosenOne
+    return chosenOne 
+   
+def probBi(phr):
+    authorsList = getAuthors()
+    probs = {}
+
+    for a in authorsList:
+	probs[a] = 0
+
+    for f in phr:
+	p = prob_phr_bi(f)
+	for author in probs:
+		probs[author] = ( probs[author] + p[author] ) * getUniqueWords(loadUnigrams(author))
+      
+    maximum = -sys.maxint - 1
+    chosenOne = ""
+    for a in authorsList:
+	if probs[a] > maximum:
+		maximum = probs[a]
+		chosenOne = a
+    print probs
+    print chosenOne
+    return chosenOne 
+
+def prob_phr_uni(frase):
+    words = frase.split()
+    authorsList = getAuthors()
+    probs = {};
+    for a in authorsList:
+        unigramsCount = loadUnigrams(a)
+        probabilidade = 1;
+        for w in words:
+	    if w in unigramsCount:
+            	value = float(unigramsCount[w])/float(getNumWords(unigramsCount))
+	    else:
+		value = 0
+            probabilidade = probabilidade * value
+        probs[a] = probabilidade
+    return probs
+ 
+ 
+def prob_phr_bi(frase):
+    words = frase.split()
+    authorsList = getAuthors()
+    probs = {};
+    for a in authorsList:
+        bgramsCount = loadBigrams(a)
+	unigramsCount = loadUnigrams(a)
+        probabilidade = 1;
+        count = 0
+        for w in words:
+	    if w == words[len(words)-1]:
+		break
+	    if w in bgramsCount:
+		if words[count+1] in bgramsCount[w]:
+            		value = (float(bgramsCount[w][words[count+1]]) + 1)/float(unigramsCount[w] + getUniqueWords(unigramsCount))
+		else:
+			value = float(1) /float(unigramsCount[w] + getUniqueWords(unigramsCount))
+	    else:
+		value = float(1) /float(getUniqueWords(unigramsCount))
+            probabilidade = probabilidade * value
+            count=count+1
+        probs[a] = probabilidade
+    return probs
+   
+   
+   
+def getAuthors():
+    authorsList = []
+    for subdirs in os.walk("target/corpora/treino"):
+        for dirs in subdirs[1]:
+            authorsList.append(dirs)
+    return authorsList
 flag = sys.argv[1]
 inFile = sys.argv[2]
 if flag == '-d':
@@ -86,5 +202,15 @@ if flag == '-d':
 	bigrams(p)
 	unigrams(p)
 elif flag == '-t':	
-	#calculate prob of file
-	laplace(inFile)
+	probBi(splitPhrases("normalized-text1.txt"))
+	probBi(splitPhrases("normalized-text2.txt"))
+	probBi(splitPhrases("normalized-text3.txt"))
+	probBi(splitPhrases("normalized-text4.txt"))
+	probBi(splitPhrases("normalized-text5.txt"))
+	probBi(splitPhrases("normalized-text6.txt"))
+
+
+
+
+
+
